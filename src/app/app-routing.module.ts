4f8a98bddc7f797da40144e7e49f917a58a44ba8
Camilo Uni <juan.1701822418@ucaldas.edu.co>
2021-04-16 14:02:35 -0500

    Creacion del login para forntend mascotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './publico/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component:InicioComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/inicio'
+  },
+  {
+    path: 'parametros',
+    loadChildren: () => import('./modulos/parametrizacion/parametrizacion.module').then(m=>m.ParametrizacionModule)
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m=>m.SeguridadModule)
+  },
+  {
+    path: '**',
+    redirectTo:'/inicio'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
